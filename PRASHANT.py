41c922c01df1dfc48baabc75f6535dc15a60d5ea
Dynamicxking <90146308+Dynamicxking@users.noreply.github.com>
2022-11-22 08:58:23 +0530

    prashant.py

---
+++ PRASHANT.py
@@ -0,0 +1,11 @@
+# prashant1\
+#python
+Thislist=["apple","banana","cherry","apple","cherry"]
+print(Thislist)
+#Boolean data
+list1=["apple","banana","cherry"]
+list2=[1,2,3,4,5,6]
+list3=[True,False,false]
+print(list1)
+print(list2)
+print(list3)
